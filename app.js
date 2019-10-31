c962bf43c0b93b356f6a1105dae1aba788d76241
ksubbumca85 <ksubbumca85@gmail.com>
2019-10-31 15:52:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 const {getHomePage} = require('./routes/index');
 const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
-//const port = 9999;
+const port = 8080;
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
